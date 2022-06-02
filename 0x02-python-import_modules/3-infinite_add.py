0cc7fe9d881bca43dbd1efe613e561135c872647
etojama <torohkiss@gmail.com>
2022-06-02 16:51:43 -0700

    added v

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     sum = 0
 
-    for args in sys.arg:
+    for args in sys.argv:
         if args != sys.arg[0]:
             sum += int(args)
     print("{}".format(sum))
