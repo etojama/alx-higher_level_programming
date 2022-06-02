fd2511a0195ec1234e631187a953866d61a98696
etojama <torohkiss@gmail.com>
2022-06-02 16:50:19 -0700

    argument additions

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sum = 0
+
+    for args in sys.arg:
+        if args != sys.arg[0]:
+            sum += int(args)
+    print("{}".format(sum))
