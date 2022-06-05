de839bdf62b4e17c04bfd98133141140ec70e106
etojama <torohkiss@gmail.com>
2022-06-05 13:38:52 -0700

    inex

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# element_at - checking element at index
+def element_at(my_list, idx):
+    if idx < 0:
+        return None
+    elif idx > len(my_list):
+        return None
+    else:
+        return my_list.index(idx)
