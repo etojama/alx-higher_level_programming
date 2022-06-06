a7215228ea9f0662f0e724c603a95269f632f6b2
etojama <torohkiss@gmail.com>
2022-06-06 08:57:09 -0700

    added else

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,5 +3,8 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0:
         return my_list
-    else:
-        return del my_list[idx]
+    else idx >= len(my_list):
+        return my_list
+    
+    del my_list[idx]
+    return my_list
