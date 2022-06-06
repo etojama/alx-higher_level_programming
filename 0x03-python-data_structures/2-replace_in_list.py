4fe1a8673af7f07a665d4b0091c41a821bbc593a
etojama <torohkiss@gmail.com>
2022-06-06 03:00:51 -0700

    removed comment

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
-# replace_in_list - replace element in a list
+
+
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
     elif idx >= len(my_list):
         return my_list
     else:
-        return mylist[idx] = element
+        return mylist.insert(idx, element)
