0a4d1ddb4e48eaf626e31e29398022a4daebd42b
etojama <torohkiss@gmail.com>
2022-06-06 02:58:19 -0700

    added elif

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,7 +4,7 @@
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    else idx >= len(my_list):
+    elif idx >= len(my_list):
         return my_list
-    mylist[idx] = element
-    return my_list
+    else:
+        return mylist[idx] = element
