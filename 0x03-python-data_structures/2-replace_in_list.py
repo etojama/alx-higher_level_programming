4f314af14f2547643798ebebab267803e3e044c9
etojama <torohkiss@gmail.com>
2022-06-06 02:45:35 -0700

    replae

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# replace_in_list - replace element in a list
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        return my_list.insert(idx, element)
