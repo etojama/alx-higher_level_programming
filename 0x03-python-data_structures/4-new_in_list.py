e744706db1ed22f496108a5cd34f9019532d47ea
etojama <torohkiss@gmail.com>
2022-06-06 03:58:10 -0700

    added copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,9 +2,9 @@
 
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return mylist[:]
+        return mylist.copy()
     elif idx >= len(my_list):
-        return my_list[:]
+        return my_list.copy()
     else:
-        new_list = my_list[:]
+        new_list = my_list.copy()
         return new_list.replace(idx, element)
