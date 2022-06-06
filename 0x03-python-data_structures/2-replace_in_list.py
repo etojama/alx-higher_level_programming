9413b12c0a027a4568d9128ef95a02e7881d2de5
etojama <torohkiss@gmail.com>
2022-06-06 03:03:47 -0700

    added doc string

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,6 +2,8 @@
 
 # replace_in_list - replace element in list
 def replace_in_list(my_list, idx, element):
+    """replace element in list"""
+
     if idx < 0:
         return my_list
     elif idx >= len(my_list):
