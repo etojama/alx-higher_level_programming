ad663736d104c4323bccf4fb18ce2f1cfb6741fe
etojama <torohkiss@gmail.com>
2022-06-06 08:45:43 -0700

    multiples of 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def divisible_by_2(my_list=[]):
+    for i in range(0, len(my_list)):
+        check = my_list[i]
+        if check % 2 == 0:
+            return True
+        else:
+            return false
