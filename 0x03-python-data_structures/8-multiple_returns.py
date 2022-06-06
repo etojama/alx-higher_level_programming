4514920fe49842214adb061ed0f5093679a72eab
etojama <torohkiss@gmail.com>
2022-06-06 08:21:32 -0700

    used comma

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,5 +5,5 @@ def multiple_returns(sentence):
     if len(sentence) == 0:
         sentence[0] = None
     else:
-        str_len = len(sentence) and sentence[0]
+        str_len = len(sentence), sentence[0]
         return str_len
