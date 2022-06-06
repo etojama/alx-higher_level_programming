da5306318b1f6ceb2ebace2cabcc2580901fb1a1
etojama <torohkiss@gmail.com>
2022-06-06 08:48:50 -0700

    added variable

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 
 def divisible_by_2(my_list=[]):
+    new_c = my_list.copy()
     for i in range(0, len(my_list)):
         check = my_list[i]
         if check % 2 == 0:
-            return True
+            new_c[i] = 1
         else:
-            return false
+            new_c[i] = 0
+    return new_c
