6df7b2eba38289c862fefb15d22a5b07bccedf1e
etojama <torohkiss@gmail.com>
2022-06-07 04:01:24 -0700

    made file executable

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0:
-        return mylist.copy()
-    elif idx >= len(my_list):
-        return my_list.copy()
-    else:
-        new_list = my_list.copy()
-        return new_list.replace(idx, element)
+    new_list = my_list[:]
+    if idx < 0 or idx >= len(my_list):
+        return new_list
+    new_list[idx] = element
+    return new_list
