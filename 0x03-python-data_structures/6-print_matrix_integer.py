ee9f17151601479b59fe721a54b1ed1567cdabbb
etojama <torohkiss@gmail.com>
2022-06-07 04:05:38 -0700

    removed first if statement

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,12 +3,10 @@
 
 # def print_matrix_integer - print matrix integer
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[i])):
-                if j != len(matrix[i]) - 1:
-                    print("{:d}".format(matrix[i][j]), end=" ")
-                else:
-                    print("{:d}".format(matrix[i][j]))
-    else:
+    for row in matrix:
+        for i in range(len(row)):
+            if i < (len(row) - 1):
+                print('{:d}'.format(row[i]), end=' ')
+            else:
+                print('{:d}'.format(row[i]), end='')
         print()
