b508046af81e13db2e5ef141bd0f3e15141f6018
etojama <torohkiss@gmail.com>
2022-06-09 07:36:21 -0700

    replace

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    my_list = my_list[:]
+
+    for i, j in enumerate(new_list):
+        if j == search:
+            my_list[i] = replace
+    return my_list
