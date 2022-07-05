c737376d00088605868f78f52be431942c550314
etojama <torohkiss@gmail.com>
2022-07-05 08:53:47 -0700

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    8-class_to_json: class_to_json()
+"""
+
+
+
+
+def class_to_json(obj):
+    """
+        returns dictionary description with simple data structure.
+    """
+    return obj.__dict__
