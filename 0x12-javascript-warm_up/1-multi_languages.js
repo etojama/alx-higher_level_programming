bae0ad10d92bb4a78adf2f041a78244c8ceeb88d
etojama <torohkiss@gmail.com>
2022-09-05 22:28:14 +0000

    used arr

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-console.log('C is fun');
-consloe.log('Python is cool');
-console.log('JavaScript is amazing');
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++){
+console.log(arr[i]);
+}
